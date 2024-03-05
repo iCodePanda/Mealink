0f43ee7283421469bd224251cd3e46cfb2acfbb7
Daniel Lu <56607543+iCodePanda@users.noreply.github.com>
2024-03-04 21:44:10 -0500

    maps request for user location permission

--- app/src/main/java/com/example/myapplication/MapsActivity.kt
+++ app/src/main/java/com/example/myapplication/MapsActivity.kt
@@ -1,33 +1,120 @@
 package com.example.myapplication
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myapplication.R
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import android.Manifest
+import android.util.Log
+import com.google.android.gms.maps.CameraUpdateFactory
 
 // Implement OnMapReadyCallback.
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+    private var locationPermissionGranted = false
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var mMap: GoogleMap
+    companion object {
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Set the layout file as the content view.
         setContentView(R.layout.activity_maps)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         // Get a handle to the fragment and register the callback.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(this)
+        getLocationPermission()
     }
 
     // Get a handle to the GoogleMap object and display marker.
     override fun onMapReady(googleMap: GoogleMap) {
-        googleMap.addMarker(
+        mMap = googleMap
+        mMap.addMarker(
             MarkerOptions()
-                .position(LatLng(0.0, 0.0))
+                .position(LatLng(0.0, 0.0)) // You might want to update this to a relevant position
                 .title("Marker")
         )
+
+        // Update location UI and get device location if permission has been granted
+        if (locationPermissionGranted) {
+            updateLocationUI(mMap)
+            getDeviceLocation()
+        }
     }
+
+
+    private fun getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        locationPermissionGranted = false
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationPermissionGranted = true
+                    updateLocationUI(mMap) // Update UI with location information
+                    getDeviceLocation() // Fetch and display the device's location
+                }
+            }
+        }
+    }
+
+
+    private fun updateLocationUI(googleMap: GoogleMap) {
+        if (locationPermissionGranted) {
+            googleMap.isMyLocationEnabled = true
+            googleMap.uiSettings.isMyLocationButtonEnabled = true
+        } else {
+            googleMap.isMyLocationEnabled = false
+            googleMap.uiSettings.isMyLocationButtonEnabled = false
+        }
+    }
+
+    private fun getDeviceLocation() {
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationClient.lastLocation
+                locationResult.addOnSuccessListener { location ->
+                    if (location != null) {
+                        mMap.moveCamera(
+                            CameraUpdateFactory.newLatLngZoom(
+                            LatLng(location.latitude, location.longitude), 15f))
+                    } else {
+                        Log.d("MapsActivity", "Current location is null. Using defaults.")
+                        mMap.uiSettings.isMyLocationButtonEnabled = false
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            e.message?.let { Log.e("Exception: %s", it) }
+        }
+    }
+
+
 }
