59c2196886f9d3433b73f269d5ad42a9694eb8b7
Teng Ma <t55ma@uwaterloo.ca>
2024-03-06 19:57:16 -0500

    Revert "added User Profile page after successful login"
    This reverts commit c0c082837b56702afbcbb6beb73353702a41c800, reversing
    changes made to 0f43ee7283421469bd224251cd3e46cfb2acfbb7.

--- app/src/main/java/com/example/myapplication/SignInActivity.kt
+++ app/src/main/java/com/example/myapplication/SignInActivity.kt
@@ -96,8 +96,7 @@ fun signIn(email: String, password: String, context: Context) {
                 // Sign in success, update UI with the signed-in user's information
                 Log.d(TAG, "signInWithEmail:success")
                 val user = auth.currentUser
-                val intent = Intent(context, UserProfileActivity::class.java)
-                context.startActivity(intent, null)
+//                updateUI(user)
             } else {
                 // If sign in fails, display a message to the user.
                 Log.w(TAG, "signInWithEmail:failure", task.exception)
