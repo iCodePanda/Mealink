98a8d561fa9870642e6360d1b7d2e42982541482
Daniel Lu <56607543+iCodePanda@users.noreply.github.com>
2024-03-14 18:59:47 -0400

    searchscreen

--- app/src/main/java/com/example/myapplication/SearchOffers.kt
+++ app/src/main/java/com/example/myapplication/SearchOffers.kt
@@ -1,4 +1,5 @@
 package com.example.myapplication
+import android.accounts.Account
 import android.content.ContentValues.TAG
 import androidx.compose.runtime.Composable
 import android.content.Context
@@ -15,6 +16,7 @@ import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.ui.Modifier
@@ -28,7 +30,9 @@ import com.google.firebase.auth.FirebaseAuth
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.sp
 import coil.compose.*
+import com.example.myapplication.ui.theme.MyApplicationTheme
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import com.google.firebase.firestore.firestore
@@ -38,4 +42,70 @@ import com.google.firebase.storage.storage
 private lateinit var auth: FirebaseAuth
 private lateinit var storage: FirebaseStorage
 class SearchOffers: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                SearchOffersScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun SearchOffersScreen() {
+    var selectedOption by remember { mutableStateOf("Using Search Bar") }
+    Column {
+        BrowseOffers()
+        CustomToggle()
+    }
+
+}
+@Composable
+fun BrowseOffers() {
+    Text(
+        "Browse Offers",
+        color = Color(0xFF00BF81),
+        fontSize = 40.sp,
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .padding(vertical = 16.dp)
+    )
+}
+
+@Composable
+fun CustomToggle() {
+
+    var selectedOption by remember { mutableStateOf("Using Search Bar") }
+    Row(modifier = Modifier.fillMaxWidth()) {
+        // Personal Account Button
+        OutlinedButton(
+            onClick = { selectedOption = "Using Search Bar" },
+            modifier = Modifier.weight(1f),
+            // Remove gap by setting border to null and elevation to 0.dp
+            border = if (selectedOption == "Using Search Bar") BorderStroke(1.dp, Color(0xFF00BF81)) else null,
+            elevation = null,
+            colors = if (selectedOption == "Using Search Bar") ButtonDefaults.buttonColors(backgroundColor = Color(0xFF00BF81)) else ButtonDefaults.buttonColors(backgroundColor = Color(0xFFF6F6F6))
+        ) {
+            Text(
+                text = "Using Search Bar",
+                color = if (selectedOption == "Using Search Bar") Color.White else Color(0xFF00BF81)
+            )
+        }
+
+        // Business Account Button
+        OutlinedButton(
+            onClick = { selectedOption = "Using Maps" },
+            modifier = Modifier.weight(1f),
+            // Remove gap by setting border to null and elevation to 0.dp
+            border = if (selectedOption == "Using Maps") BorderStroke(1.dp, Color(0xFF00BF81)) else null,
+            elevation = null,
+            colors = if (selectedOption == "Using Maps") ButtonDefaults.buttonColors(backgroundColor = Color(0xFF00BF81)) else ButtonDefaults.buttonColors(backgroundColor = Color(0xFFF6F6F6))
+        ) {
+            Text(
+                text = "Using Maps",
+                color = if (selectedOption == "Using Maps") Color.White else Color(0xFF00BF81)
+            )
+        }
+    }
 }
